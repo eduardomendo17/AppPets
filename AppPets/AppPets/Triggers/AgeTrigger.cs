6ec9bc9329d8b69602014019817e6b0b5f9a0046
Eduardo Mendoza <eduardomendo17@hotmail.com>
2021-03-01 21:10:51 -0600

    GeoLocation y Camera

---
+++ AppPets/AppPets/Triggers/AgeTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppPets.Triggers
+{
+    public class AgeTrigger : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int n;
+            bool isNumeric = int.TryParse(sender.Text, out n);
+            if (string.IsNullOrWhiteSpace(sender.Text) || !isNumeric)
+            { // No es numérico
+                sender.Text = ""; // texto anterior
+            }
+            else
+            { // Es numérico
+                if (n < 0)
+                {
+                    sender.Text = "0";
+                }
+                else if(n > 20)
+                {
+                    sender.Text = "20";
+                }
+            }
+        }
+    }
+}
--- AppPets/AppPets/Viewmodels/PetsDetailViewModel.cs
+++ AppPets/AppPets/Viewmodels/PetsDetailViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using AppPets.Models;
+using Plugin.Media;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace AppPets.Viewmodels
@@ -23,6 +25,15 @@ namespace AppPets.Viewmodels
 
         Command _DeleteCommand;
         public Command DeleteCommand => _DeleteCommand ?? (_DeleteCommand = new Command(DeleteAction));
+
+        Command _GetLocationCommand;
+        public Command GetLocationCommand=> _GetLocationCommand ?? (_GetLocationCommand = new Command(GetLocationAction));
+
+        Command _TakePictureCommand;
+        public Command TakePictureCommand => _TakePictureCommand ?? (_TakePictureCommand = new Command(TakePictureAction));
+
+        Command _SelectPictureCommand;
+        public Command SelectPictureCommand => _SelectPictureCommand ?? (_SelectPictureCommand = new Command(SelectPictureAction));
 
         /*PetModel _Pet;
         public PetModel Pet
@@ -61,6 +72,20 @@ namespace AppPets.Viewmodels
             set => SetProperty(ref _PetPicture, value);
         }
 
+        double _PetLatitude;
+        public double PetLatitude
+        {
+            get => _PetLatitude;
+            set => SetProperty(ref _PetLatitude, value);
+        }
+
+        double _PetLongitude;
+        public double PetLongitude
+        {
+            get => _PetLongitude;
+            set => SetProperty(ref _PetLongitude, value);
+        }
+
         public PetsDetailViewModel(PetsViewModel petsViewModel)
         {
             PetsViewModel = petsViewModel;
@@ -78,7 +103,7 @@ namespace AppPets.Viewmodels
             PetPicture = pet.Picture;
         }
 
-        private void SaveAction(object obj)
+        private void SaveAction()
         {
             if (_PetID > 0)
             {
@@ -114,9 +139,136 @@ namespace AppPets.Viewmodels
             Application.Current.MainPage.Navigation.PopAsync();
         }
 
-        private void DeleteAction(object obj)
+        private void DeleteAction()
         {
-            throw new NotImplementedException();
+            if (_PetID > 0)
+            {
+                // Eliminar
+
+                // Opción 1
+                List<PetModel> petsNew = new List<PetModel>();
+                foreach (PetModel pet in App.Pets)
+                {
+                    if (pet.ID != _PetID)
+                    {
+                        petsNew.Add(pet);
+                    }
+                }
+                App.Pets = petsNew;
+
+                // Opción 2
+                /*App.Pets.Remove(PetSelected);*/
+
+                PetsViewModel.PetsRefresh();
+
+                Application.Current.MainPage.Navigation.PopAsync();
+            }
+        }
+
+        private async void GetLocationAction()
+        {
+            try
+            {
+                PetLatitude = PetLongitude = 0;
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location != null)
+                {
+                    //Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    PetLatitude = location.Latitude;
+                    PetLongitude = location.Longitude;
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                await Application.Current.MainPage.DisplayAlert("AppPets", $"El GPS no está soportado en el dispositivo ({fnsEx.Message})", "Ok");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                await Application.Current.MainPage.DisplayAlert("AppPets", $"El GPS no está activado en el dispositivo ({fneEx.Message})", "Ok");
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                await Application.Current.MainPage.DisplayAlert("AppPets", $"No se pudo obtener el permiso para las coordenadas ({pEx.Message})", "Ok");
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                await Application.Current.MainPage.DisplayAlert("AppPets", $"Se generó un error al obtener las coordenadas del dispositivo ({ex.Message})", "Ok");
+            }
+        }
+
+        private async void TakePictureAction()
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await Application.Current.MainPage.DisplayAlert("AppPets", "No existe cámara disponible en el dispositivo", "Ok");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "AppPets",
+                    Name = "PetPicture.jpg"
+                });
+
+                if (file == null)
+                    return;
+
+                PetPicture = file.Path;
+
+                //await DisplayAlert("File Location", file.Path, "OK");
+                /*image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });*/
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("AppPets", $"Se generó un error al tomar la fotografía ({ex.Message})", "Ok");
+            }
+        }
+
+        private async void SelectPictureAction()
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsPickPhotoSupported)
+                {
+                    await Application.Current.MainPage.DisplayAlert("AppPets", "No es posible seleccionar fotografías en el dispositivo", "Ok");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+
+                if (file == null)
+                    return;
+
+                PetPicture = file.Path;
+
+                //await DisplayAlert("File Location", file.Path, "OK");
+                /*image.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });*/
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("AppPets", $"Se generó un error al tomar la fotografía ({ex.Message})", "Ok");
+            }
         }
 
     }
